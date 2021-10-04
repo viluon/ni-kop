20ff4137930abd4d0c78553be32b30bea305747d
Andrew Kvapil <viluon@pm.me>
2021-10-04 17:54:14 +0200

    Fix dependency management

---
+++ 1-exact-knapsack/ci.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+python3 -m venv entangled-filters
+source entangled-filters/bin/activate
+pip3 install wheel
+pip3 install wheels/*
+pip3 install -r requirements.txt
+patch entangled-filters/lib/python*/site-packages/entangled/doctest.py < doctest.patch
+cat entangled-filters/bin/pandoc-bootstrap
+make site
