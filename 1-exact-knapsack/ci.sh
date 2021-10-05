df08115ddcddaeaa3d6509c44c3d2d16d5ac35a2
Andrew Kvapil <viluon@pm.me>
2021-10-05 12:31:30 +0200

    Debug CI

--- 1-exact-knapsack/ci.sh
+++ 1-exact-knapsack/ci.sh
@@ -9,4 +9,6 @@ pip3 install wheels/*
 pip3 install -r requirements.txt
 patch entangled-filters/lib/python*/site-packages/entangled/doctest.py < doctest.patch
 cat entangled-filters/bin/pandoc-bootstrap
+mkdir -p docs
+touch docs/bench.csv
 make site
