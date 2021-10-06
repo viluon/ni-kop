dde6394dae582f6a9b7008fa864db6d23b63cd75
Andrew Kvapil <viluon@pm.me>
2021-10-06 11:57:55 +0200

    Debug CI

--- 1-exact-knapsack/ci.sh
+++ 1-exact-knapsack/ci.sh
@@ -4,7 +4,6 @@ set -euxo pipefail
 
 python3 -m venv entangled-filters
 source entangled-filters/bin/activate
-pip3 install wheel
 pip3 install wheels/*
 pip3 install -r requirements.txt
 patch entangled-filters/lib/python*/site-packages/entangled/doctest.py < doctest.patch
